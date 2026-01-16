f3361d16f81996c177fb8bb5fb60c1206d71e585
JiaHong <panjiahong@JHs-MacBook.local>
2026-01-17 02:22:08 +0800

    update blog content and layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Pan's blog";
+export const SITE_DESCRIPTION = '记录思考、创业、产品与投资';
--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,7 @@ const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
+		schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
@@ -13,6 +13,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
